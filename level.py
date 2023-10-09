713759bced5339b3f6455d2ac00f43cd6c907096
mcastre1 <miguelcastrejongal@gmail.com>
2023-10-08 20:36:24 -0600

    Player rect and x movement

--- level.py
+++ level.py
@@ -1,22 +1,39 @@
 import pygame
 from tiles import Tile
 from settings import *
+from player import Player
 
 class Level():
     def __init__(self, level_data, surface):
         # Screen from main file
         self.display_surface = surface
 
-        # Sprite group to add to when iterating through level data
+        # Sprite groups
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
 
         self.setup_level(level_data)
+
+        # Amount of pixels level is shifted on the x coordinate
+        self.world_shift = 0
         
     def setup_level(self, layout):
         for row_index, row in enumerate(layout):
             for col_index, col in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+
                 if col == 'X':
-                    self.tiles.add(Tile((col_index*tile_size, row_index*tile_size), tile_size))
+                    self.tiles.add(Tile((x, y), tile_size))
+
+                if col == 'P':
+                    self.player.add(Player((x, y)))
 
     def run(self):
+        # Level tiles
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        
+        # player
+        self.player.update()
+        self.player.draw(self.display_surface)
---
+++ player.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32,64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft = pos)
+        # Vector to keep track of player movement
+        self.direction = pygame.math.Vector2(0, 0)
+
+    # Function checks for pygame event key pressed
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        # Check for key pressed event
+        self.get_input()
+        # Move player left or right
+        self.rect.x += self.direction.x
