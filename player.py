8ac5a4bd47162926e453495f7fa1bd08f5b55de6
mcastre1 <miguelcastrejongal@gmail.com>
2023-10-08 21:50:29 -0600

    Gravity and jump

--- player.py
+++ player.py
@@ -6,10 +6,13 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
+
+        # player movement
         # Vector to keep track of player movement
         self.direction = pygame.math.Vector2(0, 0)
-
         self.speed = 8
+        self.gravity = 0.8 # Like jump speed, this has to be positive because y coordinates are positive on the lower side of screen
+        self.jump_speed = -16 # This is negative because y coordinates are positive on the lower side of screen
 
     # Function checks for pygame event key pressed
     def get_input(self):
@@ -22,8 +25,17 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    # The more the player falls, the faster the character falls
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+
     def update(self):
         # Check for key pressed event
         self.get_input()
-        # Move player left or right
-        self.rect.x += self.direction.x * self.speed
